98c7e61a63ad8f3cbda41f2765d928a2bae047fc
Robert Clayburn <rob@infosum.com>
2022-06-18 00:13:08 +0100

    add sample mesh server

--- README.md
+++ README.md
@@ -8,10 +8,20 @@ Users can use API keys to query different APIs (not oAuth for now)
 
 App logic should be modelled via xState - might need an alt visualizer linking states and transitions to web pages.
 
+## Run 
 
-## Stages
+Server
+```
+cd server 
+npm start
+```
 
-* Produce a single page designed by DnD editor, saved and loaded to file format
-* Create wrapper skeleton app to consume and host app.
-* Connect a DataList element to a predefined data source
-* 
+Client
+```
+cd client
+npm run dev
+```
+
+## Project status
+
+See https://github.com/users/pollen8/projects/1/views/1
---
+++ server/additional-resolvers.ts
@@ -0,0 +1,32 @@
+import moment from 'moment';
+
+// import type { Resolvers } from './.mesh';
+
+export const resolvers = {
+  Query: {
+    async viewsInPastMonth(root, { project }, context, info) {
+      const result = await context.Wiki.Query.getMetricsPageviewsAggregateProjectAccessAgentGranularityStartEnd({
+        root,
+        args: {
+          access: 'ALL_ACCESS',
+          agent: 'USER',
+          end: moment().format('YYYYMMDD'),
+          start: moment().startOf('month').subtract(1, 'month').format('YYYYMMDD'),
+          project,
+          granularity: 'DAILY',
+        },
+        context,
+        info,
+        selectionSet: /* GraphQL */ `
+          {
+            items {
+              views
+            }
+          }
+        `,
+      });
+
+      return result?.items?.[0]?.views || 0;
+    },
+  },
+};
